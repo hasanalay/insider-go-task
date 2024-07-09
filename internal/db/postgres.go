72b5da69639adf0f72871a6898f48e88bb479e8b
hasanalay <hasan.alay@outlook.com>
2024-07-09 15:28:55 +0300

    change folder structure to better understanding

---
+++ internal/db/postgres.go
@@ -0,0 +1,33 @@
+package db
+
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+
+	"os"
+
+	"github.com/hasanalay/insider-go-task/internal/models"
+)
+
+var DB *gorm.DB
+
+func ConnectDB() error {
+	dsn := "host=" + os.Getenv("DB_HOST") +
+		" user=" + os.Getenv("DB_USER") +
+		" password=" + os.Getenv("DB_PASS") +
+		" dbname=" + os.Getenv("DB_NAME") +
+		" port=" + os.Getenv("DB_PORT") +
+		" sslmode=" + os.Getenv("DB_SSLMODE")
+
+	var err error
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return err
+	}
+
+	if err := DB.AutoMigrate(&models.Team{}, &models.Match{}); err != nil {
+		return err
+	}
+
+	return nil
+}
